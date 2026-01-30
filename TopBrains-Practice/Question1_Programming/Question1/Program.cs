1bdeb2d6a35e8a2125e82bcdb011a748e6f5364f
Mahima Srivastava <118749197+MahimaSrivastava20@users.noreply.github.com>
2026-01-30 11:42:30 +0530

    C# Programming QuestionNo. 1

---
+++ TopBrains-Practice/Question1_Programming/Question1/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int m = int.Parse(input[0]);
+        int n = int.Parse(input[1]);
+
+        int count = 0;
+
+        for (int x = m; x <= n; x++)
+        {
+            if (!IsPrime(x))   
+            {
+                int s1 = DigitSum(x);
+                int s2 = DigitSum(x * x);
+
+                if (s2 == s1 * s1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+
+    static int DigitSum(int num)
+    {
+        int sum = 0;
+
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    static bool IsPrime(int num)
+    {
+        if (num <= 1)
+            return false;
+
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
